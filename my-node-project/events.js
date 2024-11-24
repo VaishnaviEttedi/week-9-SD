99aaf1d73e34cf246871c04cada18cc6b243e932
VaishnaviEttedi <147487582+VaishnaviEttedi@users.noreply.github.com>
2024-11-24 10:20:05 +0530

    Update events.js

--- my-node-project/events.js
+++ my-node-project/events.js
@@ -1,3 +1,6 @@
 const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
+myEmitter.on('eventName', () => {
+  console.log('An event has occurred!');
+});myEmitter.emit('eventName');
